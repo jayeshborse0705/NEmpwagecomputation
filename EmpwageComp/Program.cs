9ba9cfe9f83342113dee350847c5df1e7d31f859
jayeshborse0705 <jayeshborse0705@gmail.com>
2022-08-19 22:55:13 +0530

    [jayesh] Emp present or not

---
+++ EmpwageComp/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace EmployeeWages
+{
+    public class Program
+    {
+        public int EmpPresent = 1;
+        public void CheckEmppresentAbsent()
+        {
+            Random Check = new Random();
+            int CheckEmp = Check.Next(0, 2);
+
+            if (EmpPresent == CheckEmp)
+            {
+                Console.WriteLine("Employee is Present");
+            }
+            else
+            {
+                Console.WriteLine("Employee is Absent");
+            }
+        }
+        public static void Main(string[] args)
+        {
+            Program prg = new Program();
+            prg.CheckEmppresentAbsent();
+
+        }
+    }
+}
